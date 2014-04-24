d6e489822dd1cc069118d58189dd7034d1ac5d19
Chris Tang <chris.l.tang@gmail.com>
2014-04-24 14:00:31 -0400

    Robozzle engine

--- app/scripts/services/robozzle.js
+++ app/scripts/services/robozzle.js
@@ -57,4 +57,311 @@ angular.module('robozzleApp')
     // injectable in the DI framework.
     return( Person );
 
+  }])
+
+  .value('assert', function (assertion, message) {
+    if (!assertion) {
+      throw message || 'Assertion failed';
+    }
+  })
+
+  .value('Op', {
+
+    NOP   :  0,
+    F1    :  1,
+    F2    :  2,
+    F3    :  3,
+    F4    :  4,
+    F5    :  5,
+    FWD   :  6,
+    L90   :  7,
+    R90   :  8
+
+  })
+
+  .value('Color', {
+
+    CLEAR :  0,
+    RED   :  1,
+    BLUE  :  2,
+    GREEN :  3
+
+  })
+
+  .value('Heading', {
+
+    UP    :  0,
+    RIGHT :  1,
+    DOWN  :  2,
+    LEFT  :  3
+
+  })
+
+  .value('Material', {
+
+    TILE  :  0,
+    STAR  :  1,
+    VOID  :  2,
+    BLANK :  3
+
+  })
+
+  .factory('Step', [function __classFactory() {
+
+    function Step(op, color) {
+      this.op = op;
+      this.color = color;
+    }
+
+    function stepOf(index) {
+      var op = index % 10,
+          color = Math.floor(index / 10);
+      return new Step(op, color);
+    }
+
+    Step.encode = function (op, color) {
+      return 10 * color + op;
+    };
+
+    Step.decode = function (encoded) {
+      var decoder = _.memoize(function (e) { return stepOf(e); });
+      return decoder(encoded);
+    };
+
+    return Step;
+
+  }])
+
+  .factory('Stack', [function __classFactory() {
+
+    function Stack() {
+      this.tape = [];
+    }
+
+    Stack.prototype = {
+      any: function () {
+        return this.tape.length > 0;
+      },
+      pop: function () {
+        return this.tape.pop();
+      },
+      push: function (opCodes) {
+        this.tape = this.tape.concat(opCodes);
+      }
+    };
+
+    return Stack;
+
+  }])
+
+  .factory('Tile', ['Color', 'Material', function __classFactory(
+    Color, Material) {
+
+    function Tile(material, color) {
+      this.material = material || Material.VOID;
+      this.color = color || Color.CLEAR;
+    }
+
+    return Tile;
+
+  }])
+
+  .factory('World', ['Heading', 'Material', 'Tile', 'assert', function __classFactory(
+    Heading, Material, Tile, assert) {
+
+    function World(maxX, maxY, currentHeading, currentX, currentY) {
+      assert(currentX < maxX);
+      assert(currentY < maxY);
+      this.maxX = maxX;
+      this.maxY = maxY;
+      this.currentHeading = currentHeading;
+      this.currentX = currentX;
+      this.currentY = currentY;
+      this.safe = true;
+      this.stars = 0;
+
+      this.grid = _.map(_.range(maxY), function () {
+        return _.map(_.range(maxX), function () { 
+          return new Tile(); 
+        });
+      });
+    }
+
+    World.prototype = {
+      setTile: function (x, y, material, color) {
+        var oldMaterial = this.grid[y][x].material;
+        this.grid[y][x].material = material;
+        this.grid[y][x].color = color;
+        if (oldMaterial !== material) {
+          if (material === Material.STAR) { this.stars += 1; }
+          if (oldMaterial === Material.STAR) { this.stars -= 1; }
+        }
+      },
+      getCurrentTile: function () {
+        if (this.currentY >= 0 && this.currentY < this.maxY && this.currentX >= 0 && this.currentX < this.maxX) {
+          return this.grid[this.currentY][this.currentX];
+        } else {
+          return null;
+        }
+      },
+      advanceCursor: function () {
+
+        // don't move unless safe
+        if (!this.safe) { return; }
+
+        switch (this.currentHeading) {
+
+          case Heading.UP:
+            this.currentY -= 1;
+            break;
+
+          case Heading.RIGHT:
+            this.currentX += 1;
+            break;
+
+          case Heading.DOWN:
+            this.currentY += 1;
+            break;
+
+          case Heading.LEFT:
+            this.currentX -= 1;
+            break;
+
+        }
+
+        if (this.currentX < 0 || this.currentX >= this.maxX || this.currentY < 0 || this.currentY >= this.maxY) {
+
+          this.safe = false;
+
+        } else {
+
+          var currentTile = this.getCurrentTile();
+
+          switch (currentTile.material) {
+
+            case Material.VOID:
+              this.safe = false;
+              break;
+
+            case Material.STAR:
+              this.setTile(this.currentX, this.currentY, Material.BLANK, currentTile.color);
+              break;
+
+          }
+
+        }
+
+      },
+      rotateCursorLeft: function () {
+        // don't move unless safe
+        if (!this.safe) { return; }
+
+        this.currentHeading = ( this.currentHeading + 3 ) % 4;
+      },
+      rotateCursorRight: function () {
+        // don't move unless safe
+        if (!this.safe) { return; }
+
+        this.currentHeading = ( this.currentHeading + 1 ) % 4;
+      },
+      isComplete: function () {
+
+        return this.stars === 0;
+      }
+    };
+
+    return World;
+
+  }])
+
+  .factory('Program', ['Op', 'Stack', 'Step', 'assert', function __classFactory(
+    Op, Stack, Step, assert) {
+
+    var maxFuncs = 5,
+        maxStepsPerFunc = 10;
+
+    function Program(funcSteps) {
+      assert(funcSteps.length <= maxFuncs);
+
+      this.program = _.map(funcSteps, function __initProgram(steps) {
+        assert(steps <= maxStepsPerFunc);
+        return arrayOf(steps, Step.encode(Step.NOP, Step.CLEAR));
+      });
+
+      this.success = false;
+    }
+
+    function arrayOf(n, opCode) {
+      return _.map(_.range(n), function () { return opCode; });
+    }
+
+    Program.prototype = {
+      setFuncStep: function (fn, pos, action, color) {
+        assert(fn <= this.program.length);
+
+        var fun = this.program[fn - 1];
+            len = fun.length;
+        
+        assert(pos < len);
+
+        fun[len-pos-1] = Step.encode(action, color);
+      },
+      run: function (world) {
+        var stack = new Stack();
+        stack.push(this.program[0]);
+
+        while(stack.any() && world.safe && !world.isComplete()) {
+          var opCode = stack.pop(),
+              step = Step.decode(opCode),
+              currentTile = world.getCurrentTile();
+
+          switch (step.op) {
+
+            case Op.NOP:
+              break;
+            
+            case Op.F1:
+              if (currentTile.color === step.color) { stack.push(this.program[0]); }
+              break;
+
+            case Op.F2:
+              if (currentTile.color === step.color) { stack.push(this.program[1]); }
+              break;
+
+            case Op.F3:
+              if (currentTile.color === step.color) { stack.push(this.program[2]); }
+              break;
+
+            case Op.F4:
+              if (currentTile.color === step.color) { stack.push(this.program[3]); }
+              break;
+
+            case Op.F5:
+              if (currentTile.color === step.color) { stack.push(this.program[4]); }
+              break;
+
+            case Op.FWD:
+              if (currentTile.color === step.color) { world.advanceCursor(); }
+              break;
+
+            case Op.L90:
+              if (currentTile.color === step.color) { world.rotateCursorLeft(); }
+              break;
+
+            case Op.R90:
+              if (currentTile.color === step.color) { world.rotateCursorRight(); }
+              break;
+
+          }
+
+          if (world.safe && world.isComplete()) {
+            this.success = true;
+          }
+
+        }
+      }
+    };
+
+    return Program;
+
   }]);
--- test/spec/services/robozzle.js
+++ test/spec/services/robozzle.js
@@ -2,12 +2,21 @@
 
 describe('Service: robozzle', function () {
 
+  // load the service's module
+  beforeEach(module('robozzleApp'));
+
+  var Op, Color, Heading, Material;
+
+  beforeEach(inject(function (_Op_, _Color_, _Heading_, _Material_) {
+    Op = _Op_;
+    Color = _Color_;
+    Heading = _Heading_;
+    Material = _Material_;
+  }));
+
   describe('Factory: Person', function () {
 
-    // load the service's module
-    beforeEach(module('robozzleApp'));
-
-    // instantiate service
+    // instantiate factory
     var Person, albert, galileo;
     beforeEach(inject(function (_Person_) {
       Person = _Person_;
@@ -26,6 +35,192 @@ describe('Service: robozzle', function () {
     it('should create Person from full name', function () {
       expect(Person.fromFullName('Albert Einstein')).toEqual(albert);
       expect(Person.fromFullName('Albert Einstein')).not.toEqual(galileo);
+    });
+
+  });
+
+  describe('Factory: Step', function () {
+
+    // instantiate factory
+    var Step, randomStep, randomCode;
+
+    beforeEach(inject(function (_Step_) {
+      Step = _Step_;
+      randomStep = new Step(
+        _.random(_.keys(Op).length),
+        _.random(_.keys(Color).length)
+      );
+      randomCode = 10 * randomStep.color + randomStep.op;
+    }));
+
+    it('should encode a random step', function () {
+      expect(Step.encode(randomStep.op, randomStep.color)).toEqual(randomCode);
+    });
+
+    it('should decode a random code', function () {
+      expect(Step.decode(randomCode)).toEqual(randomStep);
+    });
+
+  });
+
+  describe('Factory: Stack', function () {
+
+    // instantiate factory
+    var Stack, someStack;
+
+    beforeEach(inject(function (_Stack_) {
+      Stack = _Stack_;
+      someStack = new Stack();
+    }));
+
+    it('should behave like a stack', function () {
+
+      expect(someStack.any()).toBe(false);
+
+      someStack.push([3, 2, 1]);
+      expect(someStack.any()).toBe(true);
+      expect(someStack.pop()).toEqual(1);
+      expect(someStack.pop()).toEqual(2);
+      expect(someStack.any()).toBe(true);
+
+      someStack.push([9, 8, 7]);
+      expect(someStack.pop()).toEqual(7);
+      expect(someStack.pop()).toEqual(8);
+      expect(someStack.pop()).toEqual(9);
+      expect(someStack.pop()).toEqual(3);
+      expect(someStack.any()).toBe(false);
+
+    });
+
+  });
+
+  describe('Factory: World', function () {
+
+    // instantiate factory
+    var World, someWorld,
+        maxX, maxY, currentHeading;
+
+    beforeEach(inject(function (_World_) {
+      World = _World_;
+      maxX = 4;
+      maxY = 3;
+      currentHeading = Heading.RIGHT;
+      someWorld = new World(maxX, maxY, currentHeading, 0, 1);
+    }));
+
+    it('should move about a trivial world', function () {
+
+      _.each(_.range(maxY), function __fillY(y) {
+        _.each(_.range(maxX), function __fillX(x) {
+          someWorld.setTile(x, y, Material.TILE, Color.CLEAR);
+        });
+      });
+
+      someWorld.advanceCursor(); // Heading.RIGHT
+      expect(someWorld.currentX).toEqual(1);
+      expect(someWorld.currentY).toEqual(1);
+
+      someWorld.rotateCursorLeft();
+      someWorld.advanceCursor(); // Heading.UP
+      expect(someWorld.currentX).toEqual(1);
+      expect(someWorld.currentY).toEqual(0);
+
+      someWorld.rotateCursorRight();
+      someWorld.advanceCursor(); // Heading.RIGHT
+      expect(someWorld.currentX).toEqual(2);
+      expect(someWorld.currentY).toEqual(0);
+
+      someWorld.rotateCursorRight();
+      someWorld.advanceCursor(); // Heading.DOWN
+      someWorld.advanceCursor(); // Heading.DOWN
+      expect(someWorld.currentX).toEqual(2);
+      expect(someWorld.currentY).toEqual(2);
+
+      someWorld.rotateCursorRight();
+      someWorld.advanceCursor(); // Heading.LEFT
+      expect(someWorld.currentX).toEqual(1);
+      expect(someWorld.currentY).toEqual(2);
+
+      someWorld.rotateCursorLeft();
+      someWorld.rotateCursorLeft();
+      someWorld.advanceCursor(); // Heading.RIGHT
+      someWorld.advanceCursor(); // Heading.RIGHT
+      expect(someWorld.currentX).toEqual(3);
+      expect(someWorld.currentY).toEqual(2);
+
+      expect(someWorld.safe).toBe(true);
+      expect(someWorld.isComplete()).toBe(true);
+      expect(someWorld.getCurrentTile().material).toEqual(Material.TILE);
+      expect(someWorld.getCurrentTile().color).toEqual(Color.CLEAR);
+
+      // now fall off 
+
+      someWorld.advanceCursor(); // too far along x
+      expect(someWorld.safe).toBe(false);
+      expect(someWorld.getCurrentTile()).toBe(null);
+
+    });
+
+    it('should not be safe when cursor hits a void', function () {
+
+      _.each(_.range(maxY), function __fillY(y) {
+        _.each(_.range(maxX), function __fillX(x) {
+          someWorld.setTile(x, y, Material.TILE, Color.CLEAR);
+        });
+      });
+
+      someWorld.setTile(1, 1, Material.VOID, Color.CLEAR);
+
+      someWorld.advanceCursor(); // Heading.RIGHT
+      expect(someWorld.safe).toBe(false);
+      expect(someWorld.currentX).toEqual(1);
+      expect(someWorld.currentY).toEqual(1);
+      expect(someWorld.getCurrentTile().material).toEqual(Material.VOID);
+      expect(someWorld.getCurrentTile().color).toEqual(Color.CLEAR);
+
+      someWorld.advanceCursor(); // can't move further from a void
+      expect(someWorld.currentX).toEqual(1);
+      expect(someWorld.currentY).toEqual(1);
+
+    });
+
+    it('should collect stars', function () {
+
+      var stars = 0;
+
+      _.each(_.range(1, maxX), function __fillX(x) {
+        stars += 1;
+        someWorld.setTile(x, 1, Material.STAR, Color.CLEAR);
+      });
+
+      expect(someWorld.isComplete()).toBe(false);
+
+      _.each(_.range(1, maxX), function __moveX() {
+        expect(someWorld.stars).toEqual(stars);
+        expect(someWorld.safe).toBe(true);
+
+        someWorld.advanceCursor();
+        stars -= 1;
+        expect(someWorld.getCurrentTile().material).toEqual(Material.BLANK);        
+      });
+
+      expect(someWorld.isComplete()).toBe(true);
+
+    });
+
+  });
+
+  describe('Factory: Program', function () {
+
+    // instantiate factory
+    var Program, theProgram;
+
+    beforeEach(inject(function (_Program_) {
+      Program = _Program_;
+    }));
+
+    describe('should run a simple program', function () {
+      
     });
 
   });
