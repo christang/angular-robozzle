8a7ca1567120abaa63e32c79c3b76eba41ab7a0a
Chris Tang <chris.l.tang@gmail.com>
2014-05-02 12:55:46 -0400

    Remove project filler

--- test/spec/services/robozzle.js
+++ test/spec/services/robozzle.js
@@ -14,31 +14,6 @@ describe('Service: robozzle', function () {
     Material = _Material_;
   }));
 
-  describe('Factory: Person', function () {
-
-    // instantiate factory
-    var Person, albert, galileo;
-    beforeEach(inject(function (_Person_) {
-      Person = _Person_;
-      albert = new Person('Albert', 'Einstein');
-      galileo = new Person('Galileo', 'Galilei');
-    }));
-
-    it('should get first name', function () {
-      expect(albert.getFirstName()).toEqual('Albert');
-    });
-
-    it('should get full name', function () {
-      expect(albert.getFullName()).toEqual('Albert Einstein');
-    });
-
-    it('should create Person from full name', function () {
-      expect(Person.fromFullName('Albert Einstein')).toEqual(albert);
-      expect(Person.fromFullName('Albert Einstein')).not.toEqual(galileo);
-    });
-
-  });
-
   describe('Factory: Step', function () {
 
     // instantiate factory
