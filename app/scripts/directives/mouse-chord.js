7b798b45f9c968c5d5921124e02ecffe658f6116
Chris Tang <chris.l.tang@gmail.com>
2014-07-12 00:49:41 -0400

    Add mouse-chord directive

---
+++ app/scripts/directives/mouse-chord.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('robozzleApp')
+
+  .directive('mouseChord', [function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, $element, $attrs) {
+
+        var handler = angular.noop, origin = {};
+
+        function resetRegistered() {
+          origin = {};
+          $element.off();
+        }
+
+        function setHandler(newHandler) {
+          handler = scope[newHandler] || angular.noop;
+        }
+
+        function reset() {
+          resetRegistered();
+          $element.on('mousedown', begin);
+        }
+
+        function update(event) {
+          scope.$apply(function () {
+            handler(event.clientX - origin.x, event.clientY - origin.y);
+          });
+        }
+
+        function begin(event) {
+          resetRegistered();
+          origin = {x: event.clientX, y: event.clientY};
+          $element.on('mouseup', reset);
+          $element.on('mousemove', update);
+        }
+
+        $attrs.$observe('onDisplacement', setHandler);
+        $element.on('mousedown', begin);
+
+      }
+    };
+  }]);
---
+++ test/spec/directives/mouse-chord.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Directive: mouseChord', function () {
+
+  // load the directive's module
+  beforeEach(module('robozzleApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+    scope.capture = function () {};
+  }));
+
+  it('should execute capture on mouse event', inject(function ($compile) {
+    element = angular.element('<div mouse-chord on-displacement="capture"></div>');
+    element = $compile(element)(scope);
+    expect(scope.dx).toBe(undefined);
+    expect(scope.dy).toBe(undefined);
+  }));
+});
